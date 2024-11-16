a411f40ff502c9956a18fe97f1432ea9dac5d2e9
MSLadanov <MSLadanov@gmail.com>
2024-11-16 10:34:09 +0300

    chore: :construction: type aliases and backend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000/api',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/static': {
+        target: 'http://localhost:5000/static',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/static/, ''),
+      },
+    },
+  },
+});
---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "UPDATE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use("/static/users", express.static(__dirname + "/static/users"));
+
+app.use(express.json());
+
+app.get("/api/v1/users", (req, res) => {
+  res.json({ name: "John", surname: "Doe" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
