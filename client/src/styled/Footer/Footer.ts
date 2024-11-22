a8610ba3da903f03dec823c8ae7571bccd8707c2
MSLadanov <MSLadanov@gmail.com>
2024-11-22 15:02:42 +0300

    chore: :iphone: fix responsive footer

--- client/src/styled/Footer/Footer.ts
+++ client/src/styled/Footer/Footer.ts
@@ -5,15 +5,19 @@ export const FooterWrapper = styled.footer`
   background: #f5f5f5;
   color: #000000;
   padding: 24px 72px;
+  display: flex;
+  justify-content: space-between; // Aligns content on larger screens
+
   @media only screen and ${devices.sm} {
-    display: flex;
     flex-direction: column;
+    align-items: center; // Center items on small screens
   }
 `;
 
 export const FooterLogo = styled.div`
   p {
-    font-family: "OpenSansLight";
+    font-family: "OpenSansLight", sans-serif; // Added fallback font
     font-size: 21px;
+    margin: 0; // Remove default margin
   }
 `;
