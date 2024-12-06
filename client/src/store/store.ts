9743863e38462c37a302c74cc48c6df58ed7d79b
MSLadanov <MSLadanov@gmail.com>
2024-12-06 09:19:21 +0300

    feat: :construction: creating user courses list

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,15 +1,26 @@
 import { makeAutoObservable } from "mobx";
 
+interface ICourse {
+  createdAt: string;
+  description: string;
+  duration: string;
+  id: number;
+  title: string;
+  updatedAt: string;
+}
+
 interface IUserStore {
   id: null | string;
   fullname: null | string;
   email: null | string;
+  courses: ICourse[];
 }
 
 class UserStore implements IUserStore {
   id = "";
   fullname = "";
   email = "";
+  courses: ICourse[] = [];
   constructor() {
     makeAutoObservable(this);
   }
@@ -22,6 +33,10 @@ class UserStore implements IUserStore {
     this.id = "";
     this.email = "";
     this.fullname = "";
+  }
+  startCourse(course: ICourse) {
+    this.courses.push(course);
+    console.log(this.courses)
   }
 }
 
